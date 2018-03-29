6751837701d5c7b8d6428149b1a096596194d6f9
Abraham Benitez Sabao <al292072@uji.es>
2018-03-29 12:10:19 +0200

    Login funcionando correctamente, hay que realizar el resto de pantallas para que una vez se haya autenticado correctamente entre en las pantallas como toca.

--- app/src/main/java/com/watchnext/watchnext/MainActivity.kt
+++ app/src/main/java/com/watchnext/watchnext/MainActivity.kt
@@ -3,21 +3,41 @@ package com.watchnext.watchnext
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import com.google.firebase.database.*
+import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.reference
+        val firestore = FirebaseFirestore.getInstance()//TODO me quede por aqui
 
         var failedLogin = true
 
         button_login.setOnClickListener {
 
             val alertDialog = AlertDialog.Builder(this).create()
-
-            if( (editText_name.length() <= 0) or (editText_passwd.length() <= 0) ) {
+            if( (editText_name.length()>0) and (editText_passwd.length()>0)){
+                firestore.collection("operarios").get().addOnSuccessListener { snapshot->
+                    for(document in snapshot.documents){
+                        if(editText_name.text.equals(document.id) and (editText_name.text.equals(document.data["pass"]))) {
+                            failedLogin = false
+                            alertDialog.setTitle("Bienvienid@ " + editText_name.text)
+                            alertDialog.setMessage("Gracias por usar WatchNext")
+                        }
+                        else{
+                            alertDialog.setTitle("Error al acceder al sistema")
+                            alertDialog.setMessage("Nombre de usuario o contraseña incorrectos")
+                        }
+                    }
+                }
+            }
+            else {
                 alertDialog.setTitle("Error al acceder al sistema")
                 var errorMessage = "Han ocurrido los siguientes errores:"
                 if (editText_name.length() <= 0)
@@ -25,16 +45,6 @@ class MainActivity : AppCompatActivity() {
                 if(editText_passwd.length() <= 0)
                     errorMessage += "\n\tLa contraseña no puede estar vacía."
                 alertDialog.setMessage(errorMessage)
-            }
-
-            else if(failedLogin) {
-                alertDialog.setTitle("Error al acceder al sistema")
-                alertDialog.setMessage("Nombre de usuario o contraseña incorrectos")
-                failedLogin = false
-            }
-            else {
-                alertDialog.setTitle("Bienvienid@ "+editText_name.text)
-                alertDialog.setMessage("Gracias por usar WatchNext")
             }
             alertDialog.show()
         }
