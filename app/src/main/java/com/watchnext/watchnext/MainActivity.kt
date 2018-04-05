d957d6452f0a549c75d334b68d5cc2105dac5a2b
Abraham Benitez Sabao <al292072@uji.es>
2018-04-05 17:17:11 +0200

    Funciona todo actualmente. Hay que pulir lo de los mensajes

--- app/src/main/java/com/watchnext/watchnext/MainActivity.kt
+++ app/src/main/java/com/watchnext/watchnext/MainActivity.kt
@@ -1,58 +1,76 @@
 package com.watchnext.watchnext
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-
+import android.util.Log
 
 
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val firestore = FirebaseFirestore.getInstance()//referencia de firestore
 
-        var failedLogin = true
+        val db = FirebaseFirestore.getInstance()//referencia de firestore
+        val operariosRef = db.collection("operarios")
+        val operarios = operariosRef.get()
+        val alertDialogBuilder = AlertDialog.Builder(this)
 
         button_login.setOnClickListener {
-
-            val alertDialog = AlertDialog.Builder(this).create()
-            if( (editText_name.length()>0) and (editText_passwd.length()>0)){
-                firestore.collection("operarios").get().addOnSuccessListener { snapshot->
-                    for(document in snapshot.documents){
-                        if(editText_name.text.equals(document.id) and (editText_name.text.equals(document.data["pass"]))) {
-                            failedLogin = false
-                            alertDialog.setTitle("Bienvienid@ " + editText_name.text)
-                            alertDialog.setMessage("Gracias por usar WatchNext")
+                Log.w("INTRODUCED", "documento: " + editText_name.text + ", pass: " + editText_passwd.text)
+                if ((editText_name.length() > 0) and (editText_passwd.length() > 0)) {
+                    var codigo =0 //0->usr y pass incorrecto, 1->pass incorrecto, 2->correcto
+                    operarios.addOnSuccessListener {snapshot ->
+                        for (document in snapshot.documents) {
+                            Log.w("ITERACION", "documento: " + document.get("id") + ", pass: " + document.get("pass"))
+                            Log.w("COMPROBANDO", "documento: " + editText_name.text.toString().equals(document.get("id").toString()) +
+                                    ", pass: " + editText_passwd.text.toString().equals(document.get("pass").toString()))
+                            if (editText_name.text.toString().equals(document.get("id").toString())) {
+                                if (editText_passwd.text.toString().equals(document.get("pass").toString())) {
+                                    val successDialogBuilder= AlertDialog.Builder(this)
+                                    Log.w("SUCCESSFUL", "documento: " + document.get("id") + ", pass: " + document.get("pass"))
+                                    successDialogBuilder.setTitle("Bienvienid@ " + editText_name.text)
+                                    successDialogBuilder.setMessage("Gracias por usar WatchNext")
+                                    successDialogBuilder.setPositiveButton("OK", DialogInterface.OnClickListener{button, whichButton ->
+                                        val intent = Intent(this, AceptarTareaActivity::class.java)
+                                        startActivity(intent,Bundle())
+                                    })
+                                    val b = successDialogBuilder.create()
+                                    b.show()
+                                    codigo=2
+                                    break
+                                }
+                                codigo=1
+                                break
+                            }
                         }
-                        else{
-                            alertDialog.setTitle("Error al acceder al sistema")
-                            alertDialog.setMessage("Nombre de usuario o contraseña incorrectos")
+                        if(codigo==0){
+                            alertDialogBuilder.setTitle("Error al acceder al sistema")
+                            alertDialogBuilder.setMessage("Nombre de usuario incorrecto")
+                        }else if(codigo==1){
+                            alertDialogBuilder.setTitle("Error al acceder al sistema")
+                            alertDialogBuilder.setMessage("Contraseña incorrecta")
                         }
                     }
+                } else {
+                    alertDialogBuilder.setTitle("Error al acceder al sistema")
+                    var errorMessage = "Han ocurrido los siguientes errores:"
+                    if (editText_name.length() <= 0)
+                        errorMessage += "\n\tEl nombre de usuario no puede estar vacío."
+                    if (editText_passwd.length() <= 0)
+                        errorMessage += "\n\tLa contraseña no puede estar vacía."
+                    alertDialogBuilder.setMessage(errorMessage)
                 }
-            }
-            else {
-                alertDialog.setTitle("Error al acceder al sistema")
-                var errorMessage = "Han ocurrido los siguientes errores:"
-                if (editText_name.length() <= 0)
-                    errorMessage += "\n\tEl nombre de usuario no puede estar vacío."
-                if(editText_passwd.length() <= 0)
-                    errorMessage += "\n\tLa contraseña no puede estar vacía."
-                alertDialog.setMessage(errorMessage)
-            }
-            alertDialog.show()
-            if(!failedLogin){
-                //TODO redireccionamos a la otra pagina
-                val intent = Intent(this, AceptarTareaActivity::class.java)
-                startActivity(intent)
-            }
+            val a = alertDialogBuilder.create()
+            a.show()
         }
-
     }
 }
