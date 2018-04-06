9bb3728e29d924120d4dbb546b15c133a322595d
Abraham Benitez Sabao <al292072@uji.es>
2018-04-06 11:44:59 +0200

    Ya funcionan correctamente los mensajes por pantalla.

--- app/src/main/java/com/watchnext/watchnext/MainActivity.kt
+++ app/src/main/java/com/watchnext/watchnext/MainActivity.kt
@@ -25,52 +25,56 @@ class MainActivity : AppCompatActivity() {
         val alertDialogBuilder = AlertDialog.Builder(this)
 
         button_login.setOnClickListener {
-                Log.w("INTRODUCED", "documento: " + editText_name.text + ", pass: " + editText_passwd.text)
-                if ((editText_name.length() > 0) and (editText_passwd.length() > 0)) {
-                    var codigo =0 //0->usr y pass incorrecto, 1->pass incorrecto, 2->correcto
-                    operarios.addOnSuccessListener {snapshot ->
-                        for (document in snapshot.documents) {
-                            Log.w("ITERACION", "documento: " + document.get("id") + ", pass: " + document.get("pass"))
-                            Log.w("COMPROBANDO", "documento: " + editText_name.text.toString().equals(document.get("id").toString()) +
-                                    ", pass: " + editText_passwd.text.toString().equals(document.get("pass").toString()))
-                            if (editText_name.text.toString().equals(document.get("id").toString())) {
-                                if (editText_passwd.text.toString().equals(document.get("pass").toString())) {
-                                    val successDialogBuilder= AlertDialog.Builder(this)
-                                    Log.w("SUCCESSFUL", "documento: " + document.get("id") + ", pass: " + document.get("pass"))
-                                    successDialogBuilder.setTitle("Bienvienid@ " + editText_name.text)
-                                    successDialogBuilder.setMessage("Gracias por usar WatchNext")
-                                    successDialogBuilder.setPositiveButton("OK", DialogInterface.OnClickListener{button, whichButton ->
-                                        val intent = Intent(this, AceptarTareaActivity::class.java)
-                                        startActivity(intent,Bundle())
-                                    })
-                                    val b = successDialogBuilder.create()
-                                    b.show()
-                                    codigo=2
-                                    break
-                                }
-                                codigo=1
+
+            Log.w("INTRODUCED", "documento: " + editText_name.text + ", pass: " + editText_passwd.text)
+            if ((editText_name.length() > 0) and (editText_passwd.length() > 0)) {
+                var codigo =0 //0->usr y pass incorrecto, 1->pass incorrecto, 2->correcto
+                operarios.addOnSuccessListener {snapshot ->
+                    for (document in snapshot.documents) {
+                        Log.w("ITERACION", "documento: " + document.get("id") + ", pass: " + document.get("pass"))
+                        Log.w("COMPROBANDO", "documento: " + editText_name.text.toString().equals(document.get("id").toString()) +
+                                ", pass: " + editText_passwd.text.toString().equals(document.get("pass").toString()))
+                        if (editText_name.text.toString().equals(document.get("id").toString())) {
+                            if (editText_passwd.text.toString().equals(document.get("pass").toString())) {
+                                val successDialogBuilder= AlertDialog.Builder(this)
+                                Log.w("SUCCESSFUL", "documento: " + document.get("id") + ", pass: " + document.get("pass"))
+                                successDialogBuilder.setTitle("Bienvienid@ " + editText_name.text)
+                                successDialogBuilder.setMessage("Gracias por usar WatchNext")
+                                successDialogBuilder.setPositiveButton("OK", DialogInterface.OnClickListener{button, whichButton ->
+                                    val intent = Intent(this, AceptarTareaActivity::class.java)
+                                    startActivity(intent,Bundle())
+                                })
+                                val b = successDialogBuilder.create()
+                                b.show()
+                                codigo=2
                                 break
                             }
-                        }
-                        if(codigo==0){
-                            alertDialogBuilder.setTitle("Error al acceder al sistema")
-                            alertDialogBuilder.setMessage("Nombre de usuario incorrecto")
-                        }else if(codigo==1){
-                            alertDialogBuilder.setTitle("Error al acceder al sistema")
-                            alertDialogBuilder.setMessage("Contraseña incorrecta")
+                            codigo=1
+                            break
                         }
                     }
-                } else {
-                    alertDialogBuilder.setTitle("Error al acceder al sistema")
-                    var errorMessage = "Han ocurrido los siguientes errores:"
-                    if (editText_name.length() <= 0)
-                        errorMessage += "\n\tEl nombre de usuario no puede estar vacío."
-                    if (editText_passwd.length() <= 0)
-                        errorMessage += "\n\tLa contraseña no puede estar vacía."
-                    alertDialogBuilder.setMessage(errorMessage)
+                    if(codigo==0){
+                        alertDialogBuilder.setMessage("Nombre de usuario incorrecto")
+                    }else if(codigo==1){
+                        alertDialogBuilder.setMessage("Contraseña incorrecta")
+                    }
+                    if(codigo!=2) {
+                        alertDialogBuilder.setTitle("Error al acceder al sistema")
+                        var b = alertDialogBuilder.create()
+                        b.show()
+                    }
                 }
-            val a = alertDialogBuilder.create()
-            a.show()
+            } else {
+                alertDialogBuilder.setTitle("Error al acceder al sistema")
+                var errorMessage = "Han ocurrido los siguientes errores:"
+                if (editText_name.length() <= 0)
+                    errorMessage += "\n\tEl nombre de usuario no puede estar vacío."
+                if (editText_passwd.length() <= 0)
+                    errorMessage += "\n\tLa contraseña no puede estar vacía."
+                alertDialogBuilder.setMessage(errorMessage)
+                var a = alertDialogBuilder.create()
+                a.show()
+            }
         }
     }
 }
